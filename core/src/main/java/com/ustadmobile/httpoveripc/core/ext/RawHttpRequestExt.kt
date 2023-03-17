3a8144da9bc6ead1be1c2cedd8e76be1abf2a6ad
Mike Dawson <mike@ustadmobile.com>
2023-03-17 17:59:17 +0400

    Fix RawHttpRequest.asSimpleTextRequest

--- core/src/main/java/com/ustadmobile/httpoveripc/core/ext/RawHttpRequestExt.kt
+++ core/src/main/java/com/ustadmobile/httpoveripc/core/ext/RawHttpRequestExt.kt
@@ -2,6 +2,7 @@ package com.ustadmobile.httpoveripc.core
 
 import com.ustadmobile.httpoveripc.core.ext.toSimpleMap
 import io.ktor.http.*
+import io.ktor.utils.io.charsets.Charsets
 import rawhttp.core.RawHttpRequest
 
 fun RawHttpRequest.asSimpleTextRequest(): SimpleTextRequest {
@@ -9,5 +10,10 @@ fun RawHttpRequest.asSimpleTextRequest(): SimpleTextRequest {
         method = SimpleTextRequest.Method.valueOf(method.uppercase()),
         url = Url(uri),
         headers = headers.toSimpleMap(),
+        requestBody = if(body.isPresent) {
+            body.get().decodeBodyToString(Charsets.UTF_8)
+        } else {
+            null
+        }
     )
 }
